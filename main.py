0123a73604224a281343b9ffd67957fe65c7b727
lelouchTheGOAT <oleh.lekhiv.ir.2023@lpnu.ua>
2023-11-10 17:20:25 +0200

    FIRST LAB

---
+++ main.py
@@ -0,0 +1,18 @@
+import math
+game = "ETS2"
+print(game)
+is_student = False
+is_teacher = True
+print(is_student and is_teacher)
+print(is_student or is_teacher)
+print(is_student is not is_teacher)
+
+
+is_raining = False
+is_snowing = False
+print(is_raining is not is_snowing)
+
+x = 2.134
+y = 0.129
+print(((x*y+(x**2)*(y**2))/math.tan((x**5)*(y**5))) - math.cos(x*y) + x*(y**(5/6)) + 173.11*x)
+
